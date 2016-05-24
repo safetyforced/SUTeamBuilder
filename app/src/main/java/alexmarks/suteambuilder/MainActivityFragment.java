556a7b50d1913b63498d58f95b634437945fbd53
Alex Marks <marks.alexanderj@gmail.com>
2016-05-24 12:11:27 -0400

    Rudimentary interface with side-by-side team lists.  Non-reponsive.

--- app/src/main/java/alexmarks/suteambuilder/MainActivityFragment.java
+++ app/src/main/java/alexmarks/suteambuilder/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mTeamAdapter1;
+    private ArrayAdapter<String> mTeamAdapter2;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,66 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        String[] teamArray1 = {
+                "Aliens",
+                "Sharks",
+                "Dragons",
+                "Wizards",
+                "Pirates",
+                "Ninjas",
+                "Princesses"
+        };
+
+        String[] teamArray2 = {
+                "Sharks",
+                "Dragons",
+                "Wizards",
+                "Pirates",
+                "Ninjas",
+                "Princesses",
+                "Aliens"
+        };
+
+        ArrayList<String> teamSet1 = new ArrayList<>(
+                Arrays.asList(teamArray1)
+        );
+
+        ArrayList<String> teamSet2 = new ArrayList<>(
+                Arrays.asList(teamArray2)
+        );
+
+
+        mTeamAdapter1 =
+                new ArrayAdapter<>(
+                        //use getActivity() as context, no idea how that works yet
+                        getActivity(),
+                        //write path to layout XML file
+                        R.layout.list_item_team1,
+                        //specify textview from layout
+                        R.id.list_item_team_textview1,
+                        //variable used to contain data (arraylist)
+                        teamSet1);
+
+        mTeamAdapter2 =
+                new ArrayAdapter<>(
+                        //use getActivity() as context, no idea how that works yet
+                        getActivity(),
+                        //write path to layout XML file
+                        R.layout.list_item_team2,
+                        //specify textview from layout
+                        R.id.list_item_team_textview2,
+                        //variable used to contain data (arraylist)
+                        teamSet2);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView1 = (ListView) rootView.findViewById(R.id.listview_team1);
+        listView1.setAdapter(mTeamAdapter1);
+
+        ListView listView2 = (ListView) rootView.findViewById(R.id.listview_team2);
+        listView2.setAdapter(mTeamAdapter2);
+
+        return rootView;
     }
 }
